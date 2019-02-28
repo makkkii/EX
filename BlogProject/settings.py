d4ee5c8de2ef9148157b75518ba08c50aad8da4a
makkkii <mohd.makki.sh@gmail.com>
2019-02-28 23:11:42 +0530

    Minor fix in runtime.txt.

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -150,10 +150,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
